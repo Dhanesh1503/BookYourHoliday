cdcef581e784e78790263974dc532a7af847edc7
dhanesh1503 <dhaneshpant@gmail.com>
2019-01-29 05:32:19 +0530

    Few updates in NPM Module configurations to remove the <Provider> '<' parser error
    Package.json + webpack configuration updated.

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,11 @@ module.exports = {
 				test : /\.(js|jsx)$/,
 				exclude : /node_modules/,
 				use : {
-					loader : 'babel-loader'
+					loader : 'babel-loader',
+					options: {
+                        presets: ['@babel/preset-env','@babel/react'],
+                        plugins: ['@babel/proposal-class-properties', '@babel/plugin-proposal-object-rest-spread', '@babel/plugin-syntax-dynamic-import']
+                    }
 				}
 			},
 			{
